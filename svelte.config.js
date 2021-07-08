825e695f857eb028fae5fa764421bfe61c0188ab
abiriadev <72962900+abiriadev@users.noreply.github.com>
2021-07-08 17:12:35 +0900

    feat: mix to express-generator
    * add svelte-preproceser
    * add svelte.config.js
    * move view to src/view

---
+++ svelte.config.js
@@ -0,0 +1,14 @@
+// svelte.config.js
+const sveltePreprocess = require('svelte-preprocess')
+const production = !process.env.ROLLUP_WATCH
+
+module.exports = {
+    // enable run-time checks when not in production
+    dev: !production,
+    // we'll extract any component CSS out into
+    // a separate file - better for performance
+    css: css => {
+        css.write('public/build/bundle.css')
+    },
+    preprocess: sveltePreprocess(),
+}
